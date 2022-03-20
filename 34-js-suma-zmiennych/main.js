3b2700403d9c227da876f9e6a6b86f9885e1e882
MikolajLiszka <mikolaj.liszka@gmail.com>
2022-03-20 12:51:08 +0100

    34-js-suma-zmiennych

---
+++ 34-js-suma-zmiennych/main.js
@@ -0,0 +1,14 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = 12;
+secondNumber = 14;
+
+function sum(num1, num2)
+{
+    let sum = num1 + num2;
+    return sum;
+}
+
+let wynik = sum(firstNumber, secondNumber); 
+console.log(wynik);
